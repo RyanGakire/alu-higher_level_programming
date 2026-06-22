3a3f7ce252d5c0a80e248d8ca8f950137c5dd239
RyanGakire <r.gakire@alustudent.com>
2026-06-22 01:53:42 -0700

    Fix task 8: Typo issue in #!/usr/bin/python3

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
-#!/usr/bin/pthon3
+#!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\ laguage that combines remarkable power with very clear syntax"
 str = str[39:66] + str[106:112] + str[0:6]
 print(str)
