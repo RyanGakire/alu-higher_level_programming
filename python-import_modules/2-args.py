0155862958569e57c9e1d3668185ff42be90f54c
RyanGakire <r.gakire@alustudent.com>
2026-06-26 13:03:41 -0700

    Python - import & modules

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-from sys import argv
-if __name_ == "__main__":
-    count = len(argv) - 1
-    if count == 0:
+if __name__ = "__main__":
+    from sys import argv
+    if __name_ == "__main__":
+        count = len(argv) - 1
+        if count == 0:
         print("0 arguments.")
     elif count == 1:
         print("1 argument:")
