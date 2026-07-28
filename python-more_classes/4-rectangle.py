f9fd14de170c297c1c947124f19cd8c3c932b9fb
RyanGakire <r.gakire@alustudent.com>
2026-07-28 15:35:47 -0700

    Python more classes Progects

---
+++ python-more_classes/4-rectangle.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python3
+"""Module that defines a Rectangle class with width and height."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """Get the current width of the rectangle.
+
+        Returns:
+            int: The width of the rectangle.
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the rectangle.
+
+        Args:
+            value (int): The new width of the rectangle.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get the current height of the rectangle.
+
+        Returns:
+            int: The height of the rectangle.
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of the rectangle.
+
+        Args:
+            value (int): The new height of the rectangle.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+    
+    def __str__(self):
+        """Return the printable string representation of the rectangle.
+
+        Returns:
+            str: The rectangle drawn with the character '#', or an
+                empty string if width or height is 0.
+        """
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        rows = []
+        for row in range(self.__height):
+            rows.append("#" * self.__width)
+        return "\n".join(rows)    
+
+    def __repr__(self):
+        """Return a string representation that can recreate the rectangle.
+
+        Returns:
+            str: A string that, if passed to eval(), creates a new
+                Rectangle instance with the same width and height.
+        """
+        return "Rectangle({}, {})".format(self.__width, self.__height)
