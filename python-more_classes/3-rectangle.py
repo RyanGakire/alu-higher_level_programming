3bde3ded8a483a3ba48f2861585e78e82fe70c57
RyanGakire <r.gakire@alustudent.com>
2026-07-29 03:55:18 -0700

    fixing trailing whitespace

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -65,7 +65,7 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value    
+        self.__height = value
 
     def area(self):
         """Return the current area of the rectangle.
