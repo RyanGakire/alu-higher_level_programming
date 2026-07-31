29a6b9c21e72397e364d0bbddc590e950ee02853
RyanGakire <r.gakire@alustudent.com>
2026-07-31 08:26:40 -0700

    Fix sql issues

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
 -- Lists all the privileges of the users user_0d_1 and user_0d_2 on localhost
--- Show the privileges of user_0d_1
+-- Show the privileges of both users
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
--- Show the privileges of user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
