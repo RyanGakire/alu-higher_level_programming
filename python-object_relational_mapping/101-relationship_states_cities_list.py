e5a44007def90dac84848bc3aa4bcc78f968abec
RyanGakire <r.gakire@alustudent.com>
2026-08-01 12:07:43 -0700

    Fix issues;

--- python-object_relational_mapping/101-relationship_states_cities_list.py
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Lists all the State objects with the cities they hold.
 
-A single query fetches the states and their cities, thanks to the
-``cities`` relationship loaded eagerly.
+The cities of a state are reached through the ``cities`` relationship
+of the State class.
 """
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import joinedload, sessionmaker
+from sqlalchemy.orm import sessionmaker
 from relationship_state import Base, State
 from relationship_city import City
 
@@ -17,10 +17,8 @@ if __name__ == "__main__":
             sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     session = sessionmaker(bind=engine)()
-    states = session.query(State).options(
-        joinedload(State.cities)).order_by(State.id).all()
-    for state in states:
+    for state in session.query(State).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
-        for city in sorted(state.cities, key=lambda city: city.id):
+        for city in state.cities:
             print("\t{}: {}".format(city.id, city.name))
     session.close()
