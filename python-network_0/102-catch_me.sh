9ddd3e8673bcf15c12048a28b739c32ceb1edde1
RyanGakire <r.gakire@alustudent.com>
2026-08-01 03:33:31 -0700

    Fix issues

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Makes a request to /catch_me, following redirects, so the server answers "You got me!"
-curl -s -L -X PUT -H "X-School-User-Id: 98" -H "Referer: HolbertonSchool" -d "user_id=98" "0.0.0.0:5000/catch_me?user_id=98"
+curl -s -L -X PUT -H "X-School-User-Id: 98" -H "Referer: https://www.holbertonschool.com" -d "user_id=98" "0.0.0.0:5000/catch_me?user_id=98"
