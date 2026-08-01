c20a9fcb6da7acf4b5f0857ac12edcfa053e2346
RyanGakire <r.gakire@alustudent.com>
2026-08-01 00:54:08 -0700

    Fix Header

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a GET request to the URL with the header X-HolbertonSchool-User-Id set to 98
-curl -s -H "X-HolbertonSchool-User-Id:98" "$1"
+curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
