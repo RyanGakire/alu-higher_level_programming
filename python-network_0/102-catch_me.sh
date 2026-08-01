8dc84f03bfd77f730ba9f8e90c11f902999ab46c
RyanGakire <r.gakire@alustudent.com>
2026-08-01 03:18:39 -0700

    Fix issues

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Makes a request to /catch_me, following redirects, so the server answers "You got me!"
-curl -s -X PATCH 0.0.0.0:5000/catch_me
+curl -s -L -X PUT 0.0.0.0:5000/catch_me
